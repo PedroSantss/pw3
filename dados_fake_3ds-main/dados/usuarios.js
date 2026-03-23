011f32e857672e6870c2e127cfb80f1f3068edd6
Pedro H. Oliveira <pedrohenrique38e6@gmail.com>
2026-03-23 06:52:05 -0300

    Add files via upload

---
+++ dados_fake_3ds-main/dados/usuarios.js
@@ -0,0 +1,35 @@
+
+const usuarios = [
+  {
+    "id": 1,
+    "Nome": "Carlos Silva",
+    "usuario": "carlos.silva",
+    "senha": "pbkdf2_sha256$12345$c4rlo5"
+  },
+  {
+    "id": 2,
+    "Nome": "Ana Oliveira",
+    "usuario": "ana_oliveira88",
+    "senha": "$2b$10$yalXnVL8tFYTklJXukJrV.INGW8Tc0cqGrG0PevwDbjwH5E6XkcI6"
+  },
+  {
+    "id": 3,
+    "Nome": "Bruno Ferreira",
+    "usuario": "bferreira_dev",
+    "senha": "$2b$10$eSB3O7nWxAEWB0vHiGdyi.rKCHAKGLqkA37axizaAvMEeC9Z7Rtd."
+  },
+  {
+    "id": 4,
+    "Nome": "Mariana Souza",
+    "usuario": "mari.souza",
+    "senha": "M4riana_Security"
+  },
+  {
+    "id": 5,
+    "Nome": "Ricardo Lima",
+    "usuario": "r.lima_admin",
+    "senha": "Admin*Pass*77"
+  }
+]
+
+export {usuarios}
